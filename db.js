7b6aa8fc9ec1ea88be452de44afe55830255bb06
Nwokocha wisdom maduabuchi <wise4rmgod@gmail.com>
2023-10-01 15:29:50 +0100

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,6 @@ const mysql = require("mysql2");
 //   user: "root", // Change to your MySQL username
 //   password: "", // Change to your MySQL password
 //   database: "movie_db",
-//   ssl: { ciphers: "DHE-RSA-AES256-SHA" },
 // });
 
 const db = mysql.createConnection({
@@ -24,6 +23,27 @@ db.connect((err) => {
     throw err;
   }
   console.log("Connected to the database");
+});
+
+const createTable = `
+  CREATE TABLE IF NOT EXISTS movies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    video_url VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    genre VARCHAR(255) NOT NULL,
+    description TEXT,
+    rating DECIMAL(3, 1),
+    release_year INT
+  )
+`;
+
+db.query(createTable, (err) => {
+  if (err) {
+    console.error("Error creating movies table:", err);
+  } else {
+    console.log("Movies table created or already exists");
+  }
 });
 
 module.exports = db;
